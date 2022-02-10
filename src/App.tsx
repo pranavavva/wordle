072f2b15a469853909951c3fcbdb198cb06d69a2
Pranav Avva <pranav.avva@gmail.com>
2022-02-10 17:41:20 -0500

    Install material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
 import * as React from 'react'
 
+import { Button, Typography } from '@mui/material'
+
 export default function App() {
-  return <div>HELLO WORLD</div>
+  const [showing, setShowing] = React.useState(false)
+
+  return (
+    <>
+      <Button variant='contained' onClick={() => setShowing(!showing)}>
+        Hello World
+      </Button>
+
+      {showing && <Typography>Hello world!</Typography>}
+    </>
+  )
 }
