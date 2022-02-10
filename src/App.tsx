779866817fe1d2465b385dfea59a2c417026a4dd
Pranav Avva <pranav.avva@gmail.com>
2022-02-10 17:27:23 -0500

    Implement absolute import. Disable eslint rule

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
+import Counter from 'features/counter/Counter'
 import logo from './logo.svg'
-import { Counter } from './features/counter/Counter'
 import './App.css'
 
 function App() {
